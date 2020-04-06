c1268831c914e5e392ec335e697c2c89cc2aa8a8
Stefan Dörger <stefan.doerger@winlocal.de>
2020-04-06 15:52:09 +0200

    Working API Search by help of UK

--- AreaT/src/app.js
+++ AreaT/src/app.js
@@ -33,33 +33,11 @@ let app = new Vue({
         get() {
             if (this.apiSearchTitle == '') return;
 
-<<<<<<< HEAD
             const apiUrl = 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/';
             let apiSearchFilm = 'film/';
 
             //let apiSearchTitle = this.movie;
             let url = apiUrl + apiSearchFilm + this.apiSearchTitle;
-=======
-        filteredPhotoFeed: function () {
-
-            let photos = this.photoFeed;
-            let authorNameSearchString = this.authorNameSearchString;
-
-            if(!authorNameSearchString){
-              return photos;
-            }
-
-            searchString = authorNameSearchString.trim().toLowerCase();
-
-            photos = photos.filter(function(item){
-              if(item.title.toLowerCase().indexOf(authorNameSearchString) !== -1){
-                return item;
-              }
-            })
-
-            return photos;
-          }
->>>>>>> 0a00498c05d163fd9e1bf25493b6972315d70c8a
 
             axios.get( url, {
                 "headers": {
@@ -73,22 +51,7 @@ let app = new Vue({
     },
 
     mounted() {
-<<<<<<< HEAD
         this.get();
-=======
-        axios.get('https://imdb-internet-movie-database-unofficial.p.rapidapi.com/', {
-            "headers": {
-                "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-                "x-rapidapi-key": "f506e8b5f3msh939f5126945ac27p103d2ajsn0011d85c92ab"
-            },
-            params: {
-                film: authorNameSearchString
-            }
-            // Film ID for search: tt7131622
-        }).then(response => {
-            this.photoFeed = response.data
-        });
->>>>>>> 0a00498c05d163fd9e1bf25493b6972315d70c8a
     }
 
 });
