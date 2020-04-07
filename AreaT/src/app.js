6f47530efcd343ecc8d021e2d6b60153a6438bac
Stefan Dörger <stefan.doerger@winlocal.de>
2020-04-07 14:52:21 +0200

    Basic Setup Search Title

--- AreaT/src/app.js
+++ AreaT/src/app.js
@@ -10,6 +10,7 @@ let app = new Vue({
         titleState: false,
         inputText: '',
         apiSearchID: '',
+        apiSearchTitle: '',
         apiFilm: [],
         // ID example: tt0475784
         // ID example 2: tt7286456
@@ -22,13 +23,21 @@ let app = new Vue({
       },
 
     methods: {
-        searchMovieID() {
+        searchMovieTitle() {
             // let apiSearchTitle = this.inputText;
-            this.get(this.apiSearchID);
+            this.getTitle(this.apiSearchID);
 
             // Title in Navbar changes from Input to film title
             this.titleState = true
         },
+
+        // searchMovieID() {
+        //     // let apiSearchTitle = this.inputText;
+        //     this.get(this.apiSearchID);
+
+        //     // Title in Navbar changes from Input to film title
+        //     this.titleState = true
+        // },
 
         exampleMovie() {
             this.apiSearchID = this.exampleInput;
@@ -42,7 +51,7 @@ let app = new Vue({
         //  this.$emit('input', value)
         // },
 
-        get(apiSearchTitle) {
+        getTitle(apiSearchTitle) {
             // let apiSearchTitle = this.apiSearchTitle;
             // if (this.apiSearchTitle == '') return;
 
@@ -61,6 +70,26 @@ let app = new Vue({
                 this.apiFilm = response.data
             });
         }
+
+        // get(apiSearchTitle) {
+        //     // let apiSearchTitle = this.apiSearchTitle;
+        //     // if (this.apiSearchTitle == '') return;
+
+        //     const apiUrl = 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/';
+        //     let apiSearchFilm = 'film/';
+
+        //     //let apiSearchTitle = this.movie;
+        //     let url = apiUrl + apiSearchFilm + apiSearchTitle;
+
+        //     axios.get( url, {
+        //         "headers": {
+        //         "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+        //         "x-rapidapi-key": "f506e8b5f3msh939f5126945ac27p103d2ajsn0011d85c92ab"
+        //         }
+        //     }).then(response => {
+        //         this.apiFilm = response.data
+        //     });
+        // }
     },
 
     mounted() {
