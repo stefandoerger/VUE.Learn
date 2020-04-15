5f50d8e075f3a5e4df7df9d03a0c9b3e1b21df8c
Stefan Dörger <stefan.doerger@winlocal.de>
2020-04-15 13:41:36 +0200

    LaraCaseet 11, Tab-Navbar, with #refs and hide content

--- src/app.js
+++ src/app.js
@@ -1,3 +1,115 @@
+Vue.component('tabs', {
+    props: [
+        'title',
+        'body'
+    ],
+
+    template: `
+
+        <div>
+
+            <ul class="nav nav-pills">
+                <li v-for="tab in tabs" class="nav-item">
+                    <a class="nav-link" :class="{ 'active': tab.isActive }" :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                </li>
+            </ul>
+
+            <div class="tabs-details">
+
+                <slot></slot>
+
+            </div>
+
+        </div>
+
+    `,
+
+    data() {
+
+        return { tabs: [] }
+
+    },
+
+    created() {
+        this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+
+});
+
+Vue.component('tab', {
+
+    template: `
+
+        <div v-show="isActive"><slot></slot></div>
+
+    `,
+
+    props: {
+        name: {required: true},
+        selected: {default: false}
+    },
+
+    data() {
+
+        return {
+            isActive: false
+        };
+
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    }
+
+});
+
+Vue.component('message', {
+    props: [
+        'title',
+        'body'
+    ],
+
+    template: `
+
+    <div v-show="isVisible">
+
+        <div>
+            <button @click="isVisible = false"> x </button>
+            <h3> {{ title }} </h3>
+        </div>
+
+
+        <div>
+            <p> {{ body }} </p>
+            <slot></slot>
+        </div>
+
+    </div>
+
+    `,
+
+    data() {
+        return {
+            isVisible: true
+        }
+    },
+
+});
+
 let app = new Vue({
     el: '#app',
 
@@ -16,6 +128,8 @@ let app = new Vue({
         apiFilm: [],
         apiSearch: [],
         urlID: '',
+
+        showModal: false
         // ID example: tt0475784
         // ID example 2: tt7286456
     },
